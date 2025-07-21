6f585d4dd5ef0c80d0565a9fa099a39c67d12551
ldnguyen06 <nguyenmax12321@gmail.com>
2025-07-21 21:50:34 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Ôi chào cậu nhía',
+    introDesc: 'Liệu tớ có thể hỏi cậu 1 câu hongg ?',
+    btnIntro: 'Okii nè',
+    title: 'Cậu có thể đii hiến máu giúp tớ trong GHTA không ạ 😙',
+    desc: 'Rấc vuii luon đó ạ',
+    btnYes: 'Gét gô thuii nè <3',
+    btnNo: 'Deo ấn được đâu :"))',
+    question:'Đồng ý rùi hả, cậu có điều gì muốn nói mí tớ khongg',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Tớ thích được hiến máu đôi mí cậuu lắmm áa',
+    mess: 'Hẹn gặp cậu ở GHTA nhé hihi😘',
+    messDesc: 'Nếu có gì bận lòng thì chia sẻ mí tớ nhéeee',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/tran.thuy.ngan.318492' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
